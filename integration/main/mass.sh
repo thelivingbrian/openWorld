99cc88e332f28d7d1313e4338893752886e0bf50
Debian <brian.lynch4444@gmail.com>
2025-01-02 20:01:16 +0000

    update gitignore add shell script

---
+++ integration/main/mass.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Loop through values of A and B
+for A in {0..1}; do
+  for B in {0..4}; do
+    for TEAM in "team-blue" "team-fuchsia"; do
+      # Construct the stagename
+      STAGENAME="${TEAM}:${A}-${B}"
+      # Execute the curl request
+      curl -X POST "http://localhost:4440/mass?stagename=${STAGENAME}&read=true&count=25&ttl=1200"
+    done
+  done
+done
+
