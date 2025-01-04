984bff77a892644fdd2bd17a9aebb89f79944a96
thelivingbrian <brian.lynch4444@gmail.com>
2025-01-03 20:00:41 -0500

    update mass to include team

--- integration/main/mass.sh
+++ integration/main/mass.sh
@@ -7,7 +7,7 @@ for A in {0..1}; do
       # Construct the stagename
       STAGENAME="${TEAM}:${A}-${B}"
       # Execute the curl request
-      curl -X POST "http://localhost:4440/mass?stagename=${STAGENAME}&read=true&count=25&ttl=1200"
+      curl -X POST "http://localhost:4440/mass?stagename=${STAGENAME}&read=true&count=25&ttl=3600&team=fuchsia"
     done
   done
 done
