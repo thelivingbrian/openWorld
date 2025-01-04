e7aadff9d9d6386f22a9403ea63b90371ce16368
thelivingbrian <brian.lynch4444@gmail.com>
2025-01-03 20:18:52 -0500

    Do both teams in shell script

--- integration/main/mass.sh
+++ integration/main/mass.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-# Loop through values of A and B
 for A in {0..1}; do
-  for B in {0..4}; do
+  for B in {0..3}; do
     for TEAM in "team-blue" "team-fuchsia"; do
       # Construct the stagename
       STAGENAME="${TEAM}:${A}-${B}"
       # Execute the curl request
-      curl -X POST "http://localhost:4440/mass?stagename=${STAGENAME}&read=true&count=25&ttl=3600&team=fuchsia"
+      curl -X POST "http://localhost:4440/mass?stagename=${STAGENAME}&read=true&count=16&ttl=3600&team=fuchsia"
+    done
+  done
+done
+
+for A in {2..3}; do
+  for B in {4..7}; do
+    for TEAM in "team-blue" "team-fuchsia"; do
+      # Construct the stagename
+      STAGENAME="${TEAM}:${A}-${B}"
+      # Execute the curl request
+      curl -X POST "http://localhost:4440/mass?stagename=${STAGENAME}&read=true&count=16&ttl=3600&team=sky-blue"
     done
   done
 done
