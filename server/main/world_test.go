31c791d3b649cba8089226a93749638e7de8c5e7
thelivingbrian <brian.lynch4444@gmail.com>
2025-01-31 23:35:13 -0500

    potential fix for occasional test lock

--- server/main/world_test.go
+++ server/main/world_test.go
@@ -14,6 +14,7 @@ import (
 )
 
 func TestSocketJoinAndMove(t *testing.T) {
+	loadFromJson()
 	world := createGameWorld(testdb())
 
 	req := createLoginRequest(PlayerRecord{Username: "test1", Y: 2, X: 2, StageName: "test-large"})
@@ -28,13 +29,15 @@ func TestSocketJoinAndMove(t *testing.T) {
 	defer testingSocket.ws.Close()
 
 	testingSocket.writeOrFatal(createInitialTokenMessage(req.Token))
-	_ = testingSocket.readOrFatal()
+	//_ = testingSocket.readOrFatal()
 
 	testingSocket.writeOrFatal(createSocketEventMessage(req.Token, "d"))
-	_ = testingSocket.readOrFatal()
+	//_ = testingSocket.readOrFatal()
 
 	testingSocket.writeOrFatal(createSocketEventMessage(req.Token, "d"))
-	_ = testingSocket.readOrFatal()
+	//_ = testingSocket.readOrFatal()
+
+	_ = testingSocket.readOrFatal() // lock risk?
 
 	// Assert
 	if len(world.worldPlayers) != 1 {
