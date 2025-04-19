b621d52b529078889e59f68311ca3e4c7b578101
thelivingbrian <brian.lynch4444@gmail.com>
2025-04-18 21:15:09 -0400

    Add Juke tests

--- server/main/player.go
+++ server/main/player.go
@@ -49,14 +49,6 @@ type Player struct {
 	hatList                  SyncHatList
 }
 
-type WebsocketConnection interface {
-	WriteMessage(messageType int, data []byte) error
-	ReadMessage() (messageType int, p []byte, err error)
-	Close() error
-	SetWriteDeadline(t time.Time) error
-	SetReadDeadline(t time.Time) error
-}
-
 ////////////////////////////////////////////////////////////
 //  Special Movement
 
