98dbeb806ad71efcae906ecf33977bb54f69f3c9
thelivingbrian <brian.lynch4444@gmail.com>
2025-04-29 17:35:20 -0400

    cleanup

--- tools/main/grid_test.go
+++ tools/main/grid_test.go
@@ -16,7 +16,6 @@ func TestGridActions(t *testing.T) {
 		snaps.MatchJSON(t, bp)
 	})
 
-	// act
 	t.Run("Test 'between' by creating a boundary.", func(t *testing.T) {
 		click1 := makeClick(1, 6, "between", "6").withSelected(1, 1)
 		click2 := makeClick(5, 6, "between", "6").withSelected(1, 6)
@@ -34,23 +33,28 @@ func TestGridActions(t *testing.T) {
 	})
 
 	t.Run("Test 'fill' against boundary.", func(t *testing.T) {
-		click1 := makeClick(2, 2, "fill", "1") // fill inner region
-		click2 := makeClick(3, 3, "fill", "2") // fill inner again - different
-		click3 := makeClick(0, 0, "fill", "4") // fill outer region
-		click4 := makeClick(1, 1, "fill", "3") // fill boundary
-		click5 := makeClick(1, 1, "fill", "4") // fill boundary to match outer
-		click6 := makeClick(2, 2, "fill", "4") // entire grid should now match
+		click1 := makeClick(2, 2, "fill", "1")  // fill inner region
+		click2 := makeClick(3, 3, "fill", "2")  // fill inner again - different
+		click3 := makeClick(0, 0, "fill", "4")  // fill outer region
+		click4 := makeClick(1, 1, "fill", "3")  // fill boundary
+		click5 := makeClick(1, 1, "fill", "4")  // fill boundary to match outer
+		click6 := makeClick(2, 2, "fill", "x4") // entire grid should now match
 
 		col.gridClickAction(click1, bp)
 		snaps.MatchSnapshot(t, bp)
+
 		col.gridClickAction(click2, bp)
 		snaps.MatchSnapshot(t, bp)
+
 		col.gridClickAction(click3, bp)
 		snaps.MatchSnapshot(t, bp)
+
 		col.gridClickAction(click4, bp)
 		snaps.MatchSnapshot(t, bp)
+
 		col.gridClickAction(click5, bp)
 		snaps.MatchSnapshot(t, bp)
+
 		col.gridClickAction(click6, bp)
 		snaps.MatchSnapshot(t, bp)
 	})
