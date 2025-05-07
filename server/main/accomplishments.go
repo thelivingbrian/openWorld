bdfb28fc3bd70cae548cfd36dcdc5cf633fa60be
thelivingbrian <brian.lynch4444@gmail.com>
2025-05-06 22:56:25 -0400

    add accomplishment list

--- server/main/accomplishments.go
+++ server/main/accomplishments.go
@@ -16,6 +16,19 @@ type Accomplishment struct {
 	// Event id? - which currently only is/could be mongo _id
 }
 
+var everyAccomplishment = []string{
+	"Become most dangerous",
+	"Score a goal",
+	"Defeat another player",
+	"10 Kill streak",
+	"100 Kill streak",
+	"1,000 money",
+	"50,000 money",
+	"Double npc kill - simultaneous",
+	"Triple npc kill - simultaneous",
+	"Puzzle 0",
+}
+
 func (accomplishments *SyncAccomplishmentList) addByName(name string) *Accomplishment {
 	accomplishments.Lock()
 	defer accomplishments.Unlock()
